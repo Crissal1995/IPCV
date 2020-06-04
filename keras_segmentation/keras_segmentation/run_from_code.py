aec1b480f23c76cc45dd826b9a71afb842c7e59e
SalvatoreCapuozzo <sasy.19.05.96@gmail.com>
2020-06-04 14:30:57 +0200

    Sviluppo delle funzionalità per pretraining e pruning di ADE20K

--- keras_segmentation/keras_segmentation/run_from_code.py
+++ keras_segmentation/keras_segmentation/run_from_code.py
@@ -51,10 +51,14 @@ from keras_segmentation.models.unet import vgg_unet
 #     return latest_epoch_checkpoint
 
 from keras_segmentation.models.model_utils import transfer_weights
+from keras_segmentation.models.model_utils import get_segmentation_model
 from keras_segmentation.pretrained import pspnet_50_ADE_20K
 from keras_segmentation.pretrained import resnet_pspnet_VOC12_v0_1
 from keras_segmentation.models.pspnet import pspnet_50
 from keras_segmentation.models.pspnet import resnet50_pspnet
+from keras_segmentation.models._pspnet_2 import Interp
+from keras.layers import Conv2D
+from keras import Input
 from keras_segmentation.models.segnet import mobilenet_segnet
 from keras import backend as K
 from keras import metrics
@@ -97,24 +101,54 @@ def jaccard_crossentropy(out, tar):
 
 #transfer_weights( pretrained_model , model  ) # transfer weights from pre-trained model to your model
 
+def convertToSunRgb(model):
+    model.trainable = False
 
-trainingMode = True
-trainingFromInit = False
-evaluate = True
+    #inputs = Input(shape=(inp.shape[1],inp.shape[2],inp.shape[3]))
+    #x = model(inputs)
+    
+    
+    inp = model.input
+    model.layers.pop()
+    model.layers.pop()
+    model.layers.pop()
+    model.layers.pop()
+    
+    #x = Conv2D(150, (1, 1), strides=(1, 1), name="conv6", padding='same')(model.layers[-1].output)
+    #x = Conv2D(37, (1, 1), strides=(1, 1), padding='same', name='conv6_sunrgb',
+                  #use_bias=False)(x)
+    #print(x)
+    #print(model.layers[-1].output)
+    x = model.layers[-1].output
+    x = Conv2D(37, (1, 1), strides=(1, 1), padding='same', name='conv6_sunrgb', use_bias=False)(x)
+    x = Interp([inp.shape[1], inp.shape[2]])(x)
+    
+    new_model = get_segmentation_model(inp,x)
+    
+    return new_model
+
+
+mode = 0
+trainingFromInit = True
+evaluate = False
 
 if os.name == 'nt':
     init = "C:/Users/UC/Desktop/"
 else:
     init = "/Users/salvatorecapuozzo/Desktop/"
 
-if trainingMode:
+if mode == 0:
     #model = fcn_32(n_classes=38 ,  input_height=224, input_width=320  )
     # model = vgg_unet(n_classes=38 ,  input_height=416, input_width=608  )
     #model = vgg_unet(n_classes=38 ,  input_height=416, input_width=608  )
     
     if trainingFromInit:
-        model = pspnet_50_slim( n_classes=38 ) # accuracy: 0.5348 10 epochs
-        opt = optimizers.Adam(learning_rate=0.003)
+        #model = pspnet_50_slim( n_classes=38 ) # accuracy: 0.5348 10 epochs
+        model = pspnet_50_ADE_20K()
+        model = convertToSunRgb(model)
+        print(model.summary())
+        
+        opt = optimizers.Adam(learning_rate=0.0001)
     else:
         checkpoints_path = init+"pspnet_50_slim_checkpoint_50/"
         full_config_path = checkpoints_path+"_config.json"
@@ -155,19 +189,19 @@ if trainingMode:
         val_images = init+"sunrgb/val/rgb/",
         val_annotations = init+"sunrgb/val/seg/",
         checkpoints_path = init,
-        batch_size = 7,
+        batch_size = 12,
         steps_per_epoch = 512,
-        val_batch_size = 8,
-        n_classes = 38,
+        val_batch_size = 12,
+        n_classes = 37,
         validate = True,
         verify_dataset = False,
         optimizer_name = opt,
-        loss_type = jaccard_crossentropy,
+        loss_type = losses.categorical_crossentropy,
         metrics_used = ['accuracy', metrics.MeanIoU(name='mean_iou', num_classes=n_classes)],
         do_augment = False,
         gen_use_multiprocessing = False,
         ignore_zero_class = False,
-        epochs = 40
+        epochs = 20
     )
     
     # print(model.summary())
@@ -175,7 +209,7 @@ if trainingMode:
     #         inp_images_dir = "/Users/salvatorecapuozzo/Desktop/sunrgb/test/rgb/", 
     #         annotations_dir = "/Users/salvatorecapuozzo/Desktop/sunrgb/test/seg/"
     #     ))
-else:
+elif mode == 1:
     #sun_inp_dir = "C:/Users/UC/Desktop/image-segmentation-keras-master/sunrgb/test/rgb/"
     #sun_ann_dir = "C:/Users/UC/Desktop/image-segmentation-keras-master/sunrgb/test/seg/"
     sun_inp_dir = "C:/Users/UC/Desktop/test/rgb/"
@@ -187,12 +221,12 @@ else:
     all_checkpoint_folders = glob.glob(checkpoints_path + "*")
     
     import matplotlib.pyplot as plt
-    fig, axes = plt.subplots(2,len(all_checkpoint_folders))
+    fig, axes = plt.subplots(1,len(all_checkpoint_folders)+2)
     img = plt.imread(sun_inp_dir+chosen_img)
-    axes[0].set_title('Original', fontsize=8)
+    axes[0].set_title('Original', fontsize=6)
     axes[0].imshow(img)
     img2 = plt.imread(sun_ann_dir+chosen_img)
-    axes[1].set_title('Segmentation', fontsize=8)
+    axes[1].set_title('Segmentation', fontsize=6)
     axes[1].imshow(img2)
     
     i = 2
@@ -223,23 +257,21 @@ else:
         
         out = model.predict_segmentation(
             inp=sun_inp_dir+chosen_img,
-            out_fname="C:/Users/UC/Desktop/ipcv_out/"+model_config['model_class']+"_"+latest_weights[-2:]+"_"+chosen_img
+            out_fname="C:/Users/UC/Desktop/ipcv_out/"+folder+"_"+latest_weights[-2:]+"_"+chosen_img
         )
         
-        
-        
         # evaluating the model 
-        if evaluate:
+        if evaluate and folder == 'pspnet_50_slim_new':
             evaluation = model.evaluate_segmentation( inp_images_dir=sun_inp_dir  , annotations_dir=sun_ann_dir ) 
             print(evaluation)
             axes[i].set_title(
                 model_config['model_class']+"_"+
                 latest_weights[-2:]+
                 "_("+str(round(100*evaluation['frequency_weighted_IU'])/100)+
-                " - "+str(round(100*evaluation['mean_IU'])/100)+")", fontsize=8)
+                " - "+str(round(100*evaluation['mean_IU'])/100)+")", fontsize=6)
             axes[i].imshow(out)
         else:
-            axes[i].set_title(model_config['model_class']+"_"+latest_weights[-2:], fontsize=8)
+            axes[i].set_title(folder+"_"+latest_weights[-2:], fontsize=6)
             axes[i].imshow(out)
             
         i += 1
@@ -249,5 +281,56 @@ else:
     plt.show()
     fig.set_size_inches(14,10)
     plt.savefig("C:/Users/UC/Desktop/ipcv_out/all_models.png")
+elif mode == 2:
+    #pretrained_model = pspnet_50_ADE_20K()
+    #model = pspnet_50( n_classes=38 ) # accuracy: 0.5348 10 epochs
     
+    #transfer_weights( pretrained_model , model  ) # transfer weights from pre-trained model to your model
+    
+    model = pspnet_50_ADE_20K()
+    model = convertToSunRgb(model)
+    print(model.summary())
+
+    sun_inp_dir = "C:/Users/UC/Desktop/test/rgb/"
+    sun_ann_dir = "C:/Users/UC/Desktop/test/seg/"
+        
+    chosen_img = "img_00027.png"
+        
+    #checkpoints_path = init+"ipcv_checkpoints/"
+    #all_checkpoint_folders = glob.glob(checkpoints_path + "*")
+    
+    
+    
+    import matplotlib.pyplot as plt
+    fig, axes = plt.subplots(1,3)
+    img = plt.imread(sun_inp_dir+chosen_img)
+    axes[0].set_title('Original', fontsize=6)
+    axes[0].imshow(img)
+    img2 = plt.imread(sun_ann_dir+chosen_img)
+    axes[1].set_title('Segmentation', fontsize=6)
+    axes[1].imshow(img2)
+    
+    out = model.predict_segmentation(
+        inp=sun_inp_dir+chosen_img,
+        out_fname="C:/Users/UC/Desktop/ipcv_out/pspne5_50_ade20k_"+chosen_img
+    )
+    
+    # evaluating the model 
+    if evaluate:
+        evaluation = model.evaluate_segmentation( inp_images_dir=sun_inp_dir  , annotations_dir=sun_ann_dir ) 
+        print(evaluation)
+        axes[2].set_title(
+            "pspnet_50_ade20k"+
+            "_("+str(round(100*evaluation['frequency_weighted_IU'])/100)+
+            " - "+str(round(100*evaluation['mean_IU'])/100)+")", fontsize=6)
+        axes[2].imshow(out)
+    else:
+        axes[2].set_title("pspnet_50_ade20k", fontsize=6)
+        axes[2].imshow(out)
+    
+    #mng = plt.get_current_fig_manager()
+    #mng.window.showMaximized()
+    plt.show()
+    fig.set_size_inches(14,10)
+    plt.savefig("C:/Users/UC/Desktop/ipcv_out/ade20k.png")
     
